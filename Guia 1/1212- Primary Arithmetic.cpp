d0f0fed73d93c1b4767844f64e43a048f9bdd50a
franco quijada <Franklynjsb>
2025-03-20 00:58:56 -0300

    Add: Ejercicios de Guía 1

--- Guia 1/1212- Primary Arithmetic.cpp
+++ Guia 1/1212- Primary Arithmetic.cpp
@@ -1,26 +1,23 @@
 #include <bits/stdc++.h>
 
-#ifdef NANO
-//local
-#else
-//judge
-#endif
-
 using namespace std;
 
-void Carry(int numero, int numero2, int &resultado) {
-	while (numero!=0 && numero2!=0){
-		if((numero%10 + numero2%10)>=10) {
-			resultado++;
-			numero/=10;
-			numero2/=10;
-			if(numero!=0) numero++;
-			if (numero2!=0) numero2++;
+void Carry(int numero, int numero2, int &carry) {
+	int resto=0, num, num2;
+	
+	while (numero > 0 || numero2 > 0){
+		num = numero%10;
+		num2 = numero2%10;
+		
+		if ((num+num2+resto)>=10){
+			resto=1;
+			carry++;
 		}else {
-			numero/=10;
-			numero2/=10;
+			resto=0;
 		}
 		
+		numero/=10;
+		numero2/=10;
 	}
 }
 
@@ -30,15 +27,22 @@ int main(int argc, char *argv[]) {
 	cin.tie(NULL);
 	cout.tie(NULL);
 	
-	int N, M, resultado=0;
-	while (cin>>N>>M, N!=0 && M!=0){
-		Carry(N,M,resultado);
-		if (resultado==0){
-			cout << "No carry operation." << endl;
+	int N, M, carry=0;
+	
+	while (cin>>N>>M, N!=0 || M!=0){
+		
+		Carry(N,M,carry);
+		
+		if (carry==0){
+			cout << "No carry operation."<< endl;;
 		}else {
-			cout << resultado << " carry operations." << endl;
+			if (carry==1) {
+				cout << carry << " carry operation."<< endl;;
+			}else {
+				cout << carry << " carry operations."<< endl;;
+			}
+			carry = 0;
 		}
-		resultado=0;
 	}
 	
 	return 0;
