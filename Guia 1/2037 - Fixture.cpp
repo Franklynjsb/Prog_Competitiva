930a4a9b0d4d83c9d0f1ad1d7dfbd0ae01a58c6e
franco quijada <Franklynjsb>
2025-03-27 22:48:01 -0300

    Fix: Correccion en ejercicios de la guía 1

--- Guia 1/2037 - Fixture.cpp
+++ Guia 1/2037 - Fixture.cpp
@@ -1,27 +1,11 @@
 #include <bits/stdc++.h>
 #define forr(i,a,b) for(int i=(a);i<(b);i++)
+#define forrIg(i,a,b) for(int i=(a);i<=(b);i++)
 #define forn(i,n) forr(i,0,n)
-#define dforn(i,n) for(int i=n-1;i>=0;i--)
+#define dforn(i,n) for(int i=n;i>=2;i--)//Cambiado para el caso >=2
 
-struct Jugador {
-	int num;
-	int lista;
-};
 
 using namespace std;
-
-void OrdenarLista(vector<Jugador> Lista){
-	int largo = Lista.size();
-	forr(i,0,largo){
-		forr(j,1,largo){
-			if (Lista[j-1].lista < Lista[j].lista){
-				Jugador aux = Lista[j-1];
-				Lista[j-1]=Lista[j];
-				Lista[j] = aux;
-			}
-		}
-	}
-}
 
 int main(int argc, char *argv[]) {
 	
@@ -31,17 +15,25 @@ int main(int argc, char *argv[]) {
 	
 	int casos;
 	while (cin>>casos, casos!=-1){
-		vector<Jugador> Listado(casos);
-		string Florencia{};
+		//casos+1 para contar a Florencia y tomar i como Nro de jugadores
+		vector<string> Listado(casos+1);
+		string Florencia;
 		
-		forr(i,1,casos){
-			cin >> Listado[i-1].lista;
-			Listado[i-1].num=i+1;
+		//Ingresa datos
+		forrIg(i,2,casos){cin>>Listado[i];}
+		
+		//Me muevo en la prob de 100 chars
+		forr(i,0,100){//The second line contains N-1 non-empty strings Li of at most100 characters each
+			//Me muevo en la lista que me dan
+			dforn(j,casos){
+				//Comparo caracteres
+				if (i<Listado[j].size() && Listado[j][i]=='1'){
+					//Asigno el jugador en Florencia
+					Florencia+= to_string(j);
+				}
+			}
 		}
 		
-		OrdenarLista(Listado);
-		
-		forr(i,0,casos){Florencia+=to_string(Listado[i].num);}
 		
 		cout << Florencia << endl;
 	}
